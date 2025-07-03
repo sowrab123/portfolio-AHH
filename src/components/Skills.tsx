06d391bb532756c7022b174e70ca28133214c7de
Sowrab Hossain <100349797+sowrab123@users.noreply.github.com>
2025-07-03 20:01:56 +0600

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,6 +20,12 @@ const Skills = () => {
       icon: <Database size={32} />,
       items: ['Django', 'Flask', 'Pandas', 'Numpy'],
       gradient: 'from-green-400 to-blue-500'
+    },
+    {
+      category: 'Cloud Computing',
+      icon: <Database size={32} />,
+      items: ['AWS', 'DigitalOcean', 'Advanced PostgreSQL', 'Docker'],
+      gradient: 'from-red-400 to-blue-500'
     }
   ];
 
